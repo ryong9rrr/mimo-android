459643e88c7fce6cb4ff0b3f3a7525fe0a48cfec
yong <tkddbs1644@gmail.com>
2024-05-12 03:44:23 +0900

    feat: while문을 사용하여 15분마다 헬스커넥트 api 호출

--- app/src/main/java/com/mimo/android/MainActivity.kt
+++ app/src/main/java/com/mimo/android/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import com.journeyapps.barcodescanner.ScanContract
 import android.os.Bundle
 import android.util.Log
+import android.util.TimeUtils
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,6 +30,7 @@ import kotlinx.coroutines.launch
 import java.time.*
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import kotlin.reflect.typeOf
 
 private const val TAG = "MainActivity"
 
@@ -143,7 +145,8 @@ class MainActivity : ComponentActivity() {
         Log.i(TAG, "App destroy")
     }
 
-
+    val dateFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        .withZone(ZoneId.of("Asia/Seoul"))
     private val scope = CoroutineScope(Dispatchers.Default)
     private var job: Job? = null
     private fun handleStartSleepForegroundService(){
@@ -165,13 +168,55 @@ class MainActivity : ComponentActivity() {
     private fun createJob(): Job{
         return scope.launch {
             while (true) {
-                val startOfDay = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS)
-                val now = Instant.now()
-                val step = healthConnectManager.readSteps(startOfDay.toInstant(), now)
-                Log.d(TAG, "MIMO가 감지 중 @@ ${getCurrentTime()} @@ ${step}")
-                delay(15 * 60 * 1000 + 30 * 1000) // 15분 30초
+                //readLastSleepStage()
+                //readSleepSession(4, 28) // 4월 28일의 기록
+                readSteps()
+                delay(15 * 60 * 1000L) // 15분
             }
         }
+    }
+
+    private suspend fun readSleepSession(
+        month: Int,
+        dayOfMonth: Int,
+    ){
+        val startTime = ZonedDateTime.of(2024, month, dayOfMonth, 0, 0, 0, 0, ZoneId.of("Asia/Seoul"))
+        val endTime = ZonedDateTime.of(2024, month, dayOfMonth, 23, 59, 59, 0, ZoneId.of("Asia/Seoul"))
+
+        val sleepSessionRecord = healthConnectManager.readSleepSessionRecord(startTime.toInstant(), endTime.toInstant())
+
+        if (sleepSessionRecord == null) {
+            Log.d(TAG, "MIMO가 감지 중")
+            Log.d(TAG, "${dateFormatter.format(startTime)} ~ ${dateFormatter.format(endTime)} 까지 수면기록 없음")
+            return
+        }
+        sleepSessionRecord.forEachIndexed() { sessionIndex, session ->
+            val koreanStartTime = dateFormatter.format(session.startTime)
+            val koreanEndTime = dateFormatter.format(session.endTime)
+            Log.d(TAG, "@@@@@@@ 상세 수면 기록 @@@@@@@")
+            Log.d(TAG, "수면 ${sessionIndex + 1} 전체 : $koreanStartTime ~ $koreanEndTime")
+            session.stages.forEach() { stage ->
+                Log.d(TAG, "${dateFormatter.format(stage.startTime)} ~ ${dateFormatter.format(stage.endTime)} @@ ${meanStage(stage.stage)}")
+            }
+        }
+    }
+
+    private suspend fun readLastSleepStage(){
+        val startOfDay = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS)
+        val now = Instant.now()
+        val lastSleepStage = healthConnectManager.readLastSleepStage(startOfDay.toInstant(), now)
+        if (lastSleepStage == null) {
+            Log.d(TAG, "MIMO가 감지 중 @@ ${getCurrentTime()} @@ 수면기록이 감지되지 않음")
+            return
+        }
+        Log.d(TAG, "MIMO가 감지 중 @@ ${getCurrentTime()} @@ ${dateFormatter.format(lastSleepStage.startTime)} ~ ${dateFormatter.format(lastSleepStage.endTime)} @@ ${meanStage(lastSleepStage.stage)}")
+    }
+
+    private suspend fun readSteps(){
+        val startOfDay = ZonedDateTime.now().truncatedTo(ChronoUnit.DAYS)
+        val now = Instant.now()
+        val step = healthConnectManager.readSteps(startOfDay.toInstant(), now)
+        Log.d(TAG, "MIMO가 감지 중 @@ ${getCurrentTime()} @@ ${step}")
     }
 
     private fun getCurrentTime(): String{
@@ -191,4 +236,43 @@ class MainActivity : ComponentActivity() {
         // 포맷에 따라 날짜 및 시간을 문자열로 변환하여 반환
         return currentTimeKST.format(formatter)
     }
+}
+
+enum class SleepStage {
+    UNKNOWN, // 0
+    AWAKE, // 1
+    SLEEPING, // 2
+    OUT_OF_BED, // 3
+    LIGHT, // 4
+    DEEP, // 5
+    REM, // 6
+    AWAKE_IN_BED // 7
+}
+
+// TODO: 수면기록은 AWAKE(수면 중 깸), LIGHT(얕은 잠), DEEP(깊은 잠), REM(렘 수면) 이렇게 4가지만 찍히는 걸로 확인됨..
+// TODO: 따라서 사실 상 이 기록이 찍히게 되면 수면이 시작됐다는 거고 AWAKE가 찍히면 수면 중 깼다는 것...
+// TODO: 그니까 이 기록이 찍히면 불 꺼주면 된다. 어차피 깨우는 건 30분 전부터 서서히 켜주면 되니까... 기상 감지는 못해도 괜찮을지도...
+fun meanStage(stage: Int): SleepStage {
+    if (stage == 1) {
+        return SleepStage.AWAKE
+    }
+    if (stage == 2) {
+        return SleepStage.SLEEPING
+    }
+    if (stage == 3) {
+        return SleepStage.OUT_OF_BED
+    }
+    if (stage == 4) {
+        return SleepStage.LIGHT
+    }
+    if (stage == 5) {
+        return SleepStage.DEEP
+    }
+    if (stage == 6) {
+        return SleepStage.REM
+    }
+    if (stage == 7) {
+        return SleepStage.AWAKE_IN_BED
+    }
+    return SleepStage.UNKNOWN
 }
