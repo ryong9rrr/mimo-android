d4fecb0f9cec287331cb1db3a6218bd00700c210
yong <tkddbs1644@gmail.com>
2024-05-16 08:25:12 +0900

    refactor: 기기상세페이지의 중복코드를 Layout 컴포넌트를 만들어서 제거

--- app/src/main/java/com/mimo/android/screens/main/myhouse/detaildevices/MyHouseLightScreen.kt
+++ app/src/main/java/com/mimo/android/screens/main/myhouse/detaildevices/MyHouseLightScreen.kt
@@ -1,27 +1,11 @@
 package com.mimo.android.screens.main.myhouse.detaildevices
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.mimo.android.apis.houses.Device
-import com.mimo.android.components.HeadingLarge
-import com.mimo.android.components.HeadingSmall
-import com.mimo.android.components.HorizontalScroll
-import com.mimo.android.components.Icon
-import com.mimo.android.components.ScrollView
-import com.mimo.android.components.base.Size
-import com.mimo.android.ui.theme.Teal100
 import com.mimo.android.viewmodels.MyHouseLightViewModel
-import com.mimo.android.viewmodels.convertDeviceTypeToKoreaName
 
 @Composable
 fun MyHouseLightScreen(
@@ -31,37 +15,13 @@ fun MyHouseLightScreen(
 ){
     val myHouseLightUiState by myHouseLightViewModel.uiState.collectAsState()
 
-    fun handleGoPrev() {
-        navController?.navigateUp()
-    }
-    BackHandler {
-        handleGoPrev()
-    }
+    Layout(
+        navController = navController,
+        device = device,
+        macAddress = myHouseLightUiState.light?.macAddress,
+        onInitializeCallback = { myHouseLightViewModel.fetchGetDevice(device) },
+        children = {
 
-    LaunchedEffect(Unit) {
-        myHouseLightViewModel.fetchGetDevice(device)
-    }
-
-    ScrollView {
-        Icon(imageVector = Icons.Filled.ArrowBack, onClick = ::handleGoPrev)
-        Spacer(modifier = Modifier.padding(14.dp))
-
-        HorizontalScroll(
-            children = {
-                HeadingLarge(text = device.nickname, fontSize = Size.lg)
-            }
-        )
-        Spacer(modifier = Modifier.padding(4.dp))
-        HorizontalScroll(
-            children = {
-                HeadingSmall(text = "MAC 주소 : ${myHouseLightUiState.light?.macAddress}", fontSize = Size.sm, color = Teal100)
-            }
-        )
-        Spacer(modifier = Modifier.padding(16.dp))
-
-        HeadingSmall(text = "${convertDeviceTypeToKoreaName(device.type)} 설정", fontSize = Size.lg)
-        Spacer(modifier = Modifier.padding(8.dp))
-
-
-    }
+        }
+    )
 }
--- app/src/main/java/com/mimo/android/screens/main/myhouse/detaildevices/MyHouseWindowScreen.kt
+++ app/src/main/java/com/mimo/android/screens/main/myhouse/detaildevices/MyHouseWindowScreen.kt
@@ -1,27 +1,11 @@
 package com.mimo.android.screens.main.myhouse.detaildevices
 
-import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.mimo.android.apis.houses.Device
-import com.mimo.android.components.HeadingLarge
-import com.mimo.android.components.HeadingSmall
-import com.mimo.android.components.HorizontalScroll
-import com.mimo.android.components.Icon
-import com.mimo.android.components.ScrollView
-import com.mimo.android.components.base.Size
-import com.mimo.android.ui.theme.Teal100
 import com.mimo.android.viewmodels.MyHouseWindowViewModel
-import com.mimo.android.viewmodels.convertDeviceTypeToKoreaName
 
 @Composable
 fun MyHouseWindowScreen(
@@ -31,37 +15,13 @@ fun MyHouseWindowScreen(
 ){
     val myHouseWindowUiState by myHouseWindowViewModel.uiState.collectAsState()
 
-    fun handleGoPrev() {
-        navController?.navigateUp()
-    }
-    BackHandler {
-        handleGoPrev()
-    }
+    Layout(
+        navController = navController,
+        device = device,
+        macAddress = myHouseWindowUiState.window?.macAddress,
+        onInitializeCallback = { myHouseWindowViewModel.fetchGetDevice(device) },
+        children = {
 
-    LaunchedEffect(Unit) {
-        myHouseWindowViewModel.fetchGetDevice(device)
-    }
-
-    ScrollView {
-        Icon(imageVector = Icons.Filled.ArrowBack, onClick = ::handleGoPrev)
-        Spacer(modifier = Modifier.padding(14.dp))
-
-        HorizontalScroll(
-            children = {
-                HeadingLarge(text = device.nickname, fontSize = Size.lg)
-            }
-        )
-        Spacer(modifier = Modifier.padding(4.dp))
-        HorizontalScroll(
-            children = {
-                HeadingSmall(text = "MAC 주소 : ${myHouseWindowUiState.window?.macAddress}", fontSize = Size.sm, color = Teal100)
-            }
-        )
-        Spacer(modifier = Modifier.padding(16.dp))
-
-        HeadingSmall(text = "${convertDeviceTypeToKoreaName(device.type)} 설정", fontSize = Size.lg)
-        Spacer(modifier = Modifier.padding(8.dp))
-
-
-    }
+        }
+    )
 }
